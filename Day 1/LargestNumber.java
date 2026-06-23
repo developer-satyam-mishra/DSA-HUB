3750b5c30c010c5e55836b025b6fba8b66cde743
Satyam <developer.satyam.mishra@proton.me>
2026-06-23 14:39:15 +0530

    Largest Number

---
+++ Day 1/LargestNumber.java
@@ -0,0 +1,14 @@
+class LargestNumber{
+    public static void main(String args[]){
+        int a=74,b=87,c=258;
+        if(a>b && a>c){
+            System.out.print("a is largest number");
+        }
+        else if(b>a && b>c){
+            System.out.print("b is largest number");
+        }
+        else{
+            System.out.print("c is largest number");
+        }
+    }
+}
