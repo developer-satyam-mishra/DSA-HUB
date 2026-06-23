9d4926f8773279e7ddde90e3378565168f923849
Satyam <developer.satyam.mishra@proton.me>
2026-06-23 14:29:24 +0530

    EVEN ODD

---
+++ Day 1/EvenOdd.java
@@ -0,0 +1,11 @@
+class EvenOdd{
+    public static void main(String args[]){
+        int number=89461;
+        if(number%2==0){
+            System.out.print("Number is Even");
+        }
+        else{
+            System.out.print("Number is Odd");
+        }
+    }
+}
